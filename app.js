c1f4f93aa35c3d87612b3342ff1db6a3320198f5
cherishma2124 <cherishmacherry27@gmail.com>
2024-12-11 01:30:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ app.get('/', (req, res) => {
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(Server running at http://0.0.0.0:${PORT});
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
